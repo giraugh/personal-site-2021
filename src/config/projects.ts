4651551af89da3151426d50b7690841c836843c7
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-05-31 15:52:55 +1000

    Update CDN project addresses

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -2,31 +2,31 @@ export const projects = [
   {
     name: 'Honey Heist',
     description: 'Online Character viewer for the open source Honey Heist RPG.',
-    image: 'http://cdn.ewanb.me/honey-heist-shot',
+    image: 'https://cdn.giraugh.xyz/honey-heist-shot',
     link: 'http://ewanb.me/honey-heist',
   },
   {
     name: 'Critical Counter',
     description: 'Discord bot for counting stats in a DnD Campaign.',
-    image: 'http://cdn.ewanb.me/critical-counter-shot',
+    image: 'https://cdn.giraugh.xyz/critical-counter-shot',
     link: 'http://github.com/giraugh/critical-counter',
   },
   {
     name: 'Stevent',
     description: 'Student Event platform managed and developed by CSIT',
-    image: 'http://cdn.ewanb.me/stevent-shot',
+    image: 'https://cdn.giraugh.xyz/stevent-shot',
     link: 'http://stevent.club',
   },
   {
     name: 'Shaders',
     description: 'My collection of Raymarched 3D Shaders.',
-    image: 'http://cdn.ewanb.me/shaders-short-shot',
+    image: 'https://cdn.giraugh.xyz/shaders-short-shot',
     link: 'https://shadertoy.com/user/giraugh',
   },
   {
     name: 'Hex Redux',
     description: 'Online tool for creating javascript bots that play Hex.',
-    image: 'http://cdn.ewanb.me/hex-redux-zoom-shot',
+    image: 'https://cdn.giraugh.xyz/hex-redux-zoom-shot',
     link: 'http://ewanb.me/hex-redux',
   },
 ]
