337390104bbcc49a9a2fceba2f2c3adb9aa9f096
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-06-08 15:41:26 +1000

    Update email address

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,6 +1,6 @@
 export const resumeLink =
   'https://drive.google.com/file/d/1u6UoSsEBk9zjqBOLg4MVTYfqV_b6yL_m/view?usp=sharing'
 export const githubLink = 'https://github.com/giraugh'
-export const contactMailAddress = 'ebreakey+contact@gmail.com'
+export const contactMailAddress = 'contact@ewanb.me'
 export const storeLink = 'https://www.redbubble.com/people/Giraugh/shop'
 export const siteGithubRepo = 'https://github.com/giraugh/personal-site-2021'
